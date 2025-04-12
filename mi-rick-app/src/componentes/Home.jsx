1d6b9d4b40c0fb9be2e32be6635e3415150a8480
PatriciaAlEs <patriciaalvarezestevez@gmail.com>
2025-04-12 17:43:44 +0200

    componente Home.jsx

---
+++ mi-rick-app/src/componentes/Home.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Home() {
+    return (
+        <div style={{ padding: "2rem" }}>
+            <h1>Gestión de Posts 📝</h1>
+        </div>
+    );
+}
+
+export default Home;
